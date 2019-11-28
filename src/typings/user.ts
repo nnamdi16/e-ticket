a4719d4c9bb455654d1bef341fbb07205ef5e3e0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-28 08:23:28 +0100

    Removed the password, add hash and salt and added an optional key

--- src/typings/user.ts
+++ src/typings/user.ts
@@ -4,9 +4,11 @@ export interface UserType extends Document {
   firstName: string;
   lastName: string;
   username: string;
-  password: string;
   email: string;
   isAdmin: boolean;
   isSuper: boolean;
   deleted: boolean;
+  hash: string;
+  salt: string;
+  [key: string]: any;
 }
